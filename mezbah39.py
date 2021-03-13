e0ffb4467e264db4ce1dfe8a51406427ff4a82e5
Mezbah uddin(davide) <75944104+davide666777@users.noreply.github.com>
2021-03-14 01:08:39 +0800

    Add files via upload

---
+++ mezbah39.py
@@ -0,0 +1,14 @@
+import requests
+import pickle
+
+data = requests.get("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/").text
+#print(data)
+
+l1 = data.split("\n")
+#print(l1)
+
+l2 = [item.split(",") for item in l1 if len(item) != 0]
+print(l2)
+
+with open("myiris.pkl","wb") as f:
+    pickle.dump(l1,f)
